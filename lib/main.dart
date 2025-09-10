c7ef3dc7cf99056eadaf65891653e873613ff485
Usama Sarwar <UsamaSarwarOfficial@gmail.com>
2025-09-10 23:16:41 +0500

    Scaffold app with Provider, navigation, and basic screens

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+	runApp(const BusinessListApp());
+}
+
+class BusinessListApp extends StatelessWidget {
+	const BusinessListApp({super.key});
+
+	@override
+	Widget build(BuildContext context) {
+		return MultiProvider(
+			providers: [
+				// Add providers here
+			],
+			child: MaterialApp(
+				title: 'Business List',
+				theme: ThemeData(
+					primarySwatch: Colors.blue,
+				),
+				initialRoute: '/',
+				routes: {
+					'/': (context) => const BusinessListScreen(),
+					'/details': (context) => const BusinessDetailsScreen(),
+				},
+			),
+		);
+	}
+}
+
+class BusinessListScreen extends StatelessWidget {
+	const BusinessListScreen({super.key});
+
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			appBar: AppBar(title: const Text('Businesses')),
+			body: const Center(child: Text('Business list will appear here.')),
+		);
+	}
+}
+
+class BusinessDetailsScreen extends StatelessWidget {
+	const BusinessDetailsScreen({super.key});
+
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			appBar: AppBar(title: const Text('Business Details')),
+			body: const Center(child: Text('Business details will appear here.')),
+		);
+	}
+}
